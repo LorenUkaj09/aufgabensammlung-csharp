f59b9c46fd48f3db31f7ce2cfebe556334c2e0c0
Loren Ukaj <Loren.ukaj@hotmail.com>
2025-09-29 08:28:34 +0100

    Projektdateien hinzufügen.

---
+++ Aufgabe -1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Zahl1;
+            int Zahl2;
+            // Die Methode WriteLine() fügt am Ende einen Zeilenumbruch ein
+         Console.WriteLine("Dieses Programm berechnet die Summe von zwei Zahlen.");
+
+        Console.Write("Zahl 1: ");
+        Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+         Console.Write("Zahl 2: ");
+        Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+       
+         double sum = Zahl1 + Zahl2;
+
+         Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ Aufgabe-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Berechnung von Sekunden eines Monats in Abhängigkeit seiner Anzahl tage");
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine(" Wieviele Tage hat der Monat, für den sie die sekundenzahl berechnen wollen?");
+            int Tage = Convert.ToInt32(Console.ReadLine());
+
+            int sektag  = 3600 * 24;
+            int sekmonat = sektag * Tage;
+            int minmonat = 28;
+            int maxmonat = 31;
+
+            if (Tage < minmonat || Tage > maxmonat)
+            {
+                Console.WriteLine("Ungültige Eingabe " + Tage + " gibt es nicht");
+            }
+            else
+            {
+                Console.WriteLine("Ein monat mit" + Tage + "Tagen hat" + sekmonat + "Sekunden" );
+
+            }
+
+            }
+
+
+           
+        }
+    }
+
