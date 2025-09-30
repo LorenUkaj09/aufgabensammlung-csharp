dc0d8c87bfb0adc01026cacd1cab813381705044
Loren Ukaj <Loren.ukaj@hotmail.com>
2025-09-30 16:07:02 +0100

    Aufgabe-12

---
+++ Aufgabe-12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_12
+{
+    internal class Program
+    {
+       
+        
+        static int[] SumUp(int[] arr)
+        {
+
+
+            int[] result = new int[arr.Length];
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                result[i] = sum;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Zahlen Aufsummieren");
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Geben sie die zu summierenden Ganzzahlen mit komma getrennt ein :");
+
+            string eingabe = Console.ReadLine();
+            string[] inputArray = eingabe.Split(',');
+            int[] zahlen = inputArray.Select(s =>  int.Parse(s.Trim())).ToArray();
+            int[] aufsummiert = SumUp(zahlen);
+
+
+            Console.WriteLine("Eingabe: " + string.Join(", ", zahlen));
+            Console.WriteLine("Aufsummiert: " + string.Join(", ", aufsummiert));
+        }
+    }   
+}
