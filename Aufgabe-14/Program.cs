b1df6d334d640fa466e4a3fc8fb9c0597fad7c24
Loren Ukaj <Loren.ukaj@hotmail.com>
2025-10-01 11:16:32 +0100

    Aufgabe-13

---
+++ Aufgabe-14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Aufgabe_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prüfen,ob es sich bei einem jahr um ein Schaltjahr handelt");
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.WriteLine("Eingabe Jahr ( q zum Beenden):");
+                string input = Console.ReadLine();
+
+                if (input?.ToLower() == "q")
+                {
+                    break;
+                }
+
+                if (int.TryParse(input, out int eingabe))
+                {
+                    if ((eingabe % 4 == 0 && eingabe % 100 != 0) || eingabe % 400 == 0)
+                    {
+                        Console.WriteLine(eingabe + " ist ein Schaltjahr");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(eingabe + " ist kein Schaltjahr");
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl ein oder 'q' zum Beenden.");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
