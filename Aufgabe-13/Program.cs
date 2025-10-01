b8e5e48e9295fda235bb2b82e5c5763efd953531
Loren Ukaj <Loren.ukaj@hotmail.com>
2025-10-01 10:52:56 +0100

    Aufgabe-12

---
+++ Aufgabe-13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aufgabe_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie lang soll die Diagonale sein?");
+            Console.Write("Deine Eingabe: ");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+
+            for (int zeile = 0; zeile < lenght; zeile++)
+            {
+                for (int spalte = 0; spalte < lenght; spalte++)
+                {
+                    if (spalte == zeile)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
+
+
